643f5db776de9e7d7a6078afdeca54be441a5374
Hao Kai <haokai@njxds.com>
2020-06-18 16:17:02 +0800

    Page 118

--- Chapter3/ActivityTest/app/src/main/java/com/example/haokai/activitytest/FirstActivity.kt
+++ Chapter3/ActivityTest/app/src/main/java/com/example/haokai/activitytest/FirstActivity.kt
@@ -1,9 +1,11 @@
 package com.example.haokai.activitytest
 
+import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -20,9 +22,26 @@ class FirstActivity : AppCompatActivity() {
             //val intent = Intent(this, SecondActivity::class.java)
             //val intent = Intent("com.example.haokai.activitytest.ACTION_START")
             //intent.addCategory("com.example.haokai.activitytest.MY_CATEGORY")
-            val intent = Intent(Intent.ACTION_VIEW)
-            intent.data = Uri.parse("https://www.baidu.com")
-            startActivity(intent)
+            //val intent = Intent(Intent.ACTION_VIEW)
+            //intent.data = Uri.parse("https://www.baidu.com")
+            //val intent = Intent(Intent.ACTION_DIAL)
+            //intent.data = Uri.parse("tel:10086")
+            //val data = "Hello SecondActivity"
+            //val intent = Intent(this, SecondActivity::class.java)
+            //intent.putExtra("extra_data", data)
+            //startActivity(intent)
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivityForResult(intent, 1)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when(requestCode){
+            1 -> if(resultCode == Activity.RESULT_OK){
+                val returnedData = data?.getStringExtra("data_return")
+                Log.d("FirstActivity", "returned data is $returnedData")
+            }
         }
     }
 
--- Chapter3/ActivityTest/app/src/main/java/com/example/haokai/activitytest/SecondActivity.kt
+++ Chapter3/ActivityTest/app/src/main/java/com/example/haokai/activitytest/SecondActivity.kt
@@ -1,11 +1,30 @@
 package com.example.haokai.activitytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.second_layout.*
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_layout)
+        //val extraData = intent.getStringExtra("extra_data")
+        //Log.d("SecondaryActivity", "extra data is $extraData")
+        button2.setOnClickListener{
+            val intent = Intent()
+            intent.putExtra("data_return", "Hello FirstActivity")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        Log.d("SecondActivity", "back pressed.")
+        val intent = Intent()
+        intent.putExtra("data_return", "Hello FirstActivity")
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
