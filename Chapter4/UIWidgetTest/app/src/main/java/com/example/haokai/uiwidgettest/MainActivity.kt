0ffd87773f4bbdeae6dfce654ae362c4d6f1087a
Kai <haokai.union@outlook.com>
2020-06-26 16:56:13 +0800

    Page 169

--- Chapter4/UIWidgetTest/app/src/main/java/com/example/haokai/uiwidgettest/MainActivity.kt
+++ Chapter4/UIWidgetTest/app/src/main/java/com/example/haokai/uiwidgettest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -11,10 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener{
+        //button.setOnClickListener{
             //logic
             //Toast.makeText(this, editText.text.toString(), Toast.LENGTH_SHORT).show()
-            imageView.setImageResource(R.drawable.img_2)
+            //imageView.setImageResource(R.drawable.img_2)
             /*
             if(progressBar.visibility == View.VISIBLE){
                 progressBar.visibility = View.GONE
@@ -23,7 +24,17 @@ class MainActivity : AppCompatActivity() {
                 progressBar.visibility = View.VISIBLE
             }
             */
-            progressBar.progress += 10
-        }
+            //progressBar.progress += 10
+/*
+            AlertDialog.Builder(this).apply {
+                setTitle("This is Dialog")
+                setMessage("Something important")
+                setCancelable(false)
+                setPositiveButton("OK"){dialog, which ->  }
+                setNegativeButton("Cancel"){dialog, which -> }
+                show()
+            }*/
+        //}
+
     }
 }
