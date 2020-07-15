5cab4cc92c1d26fb5cb3170a9bdd707169b92b87
Hao Kai <haokai@njxds.com>
2020-07-15 17:22:11 +0800

    page 366

--- Chapter9/NotificationTest/app/src/main/java/com/example/haokai/notificationtest/MainActivity.kt
+++ Chapter9/NotificationTest/app/src/main/java/com/example/haokai/notificationtest/MainActivity.kt
@@ -22,15 +22,17 @@ class MainActivity : AppCompatActivity() {
             val channel = NotificationChannel("normal", "Normal",
                     NotificationManager.IMPORTANCE_DEFAULT)
             manager.createNotificationChannel(channel)
+            val channel2 = NotificationChannel("important", "Important",
+                    NotificationManager.IMPORTANCE_HIGH)
+            manager.createNotificationChannel(channel2)
         }
         sendNotice.setOnClickListener {
             val intent = Intent(this, NotificationActivity::class.java)
             val pi = PendingIntent.getActivity(this, 0, intent, 0)
-            val notification = NotificationCompat.Builder(this, "normal")
+            val notification = NotificationCompat.Builder(this, "important")
                     .setContentTitle("This is content title")
-                    .setStyle(NotificationCompat.BigTextStyle().bigText("Learn how to build" +
-                            "notifications, send and sync data, an use voice actions. Get the" +
-                            "official Android IDE and developer tools to build apps for Android."))
+                    .setStyle(NotificationCompat.BigPictureStyle().bigPicture(BitmapFactory
+                            .decodeResource(resources, R.drawable.big_image)))
                     .setSmallIcon(R.drawable.small_icon)
                     .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.large_icon))
                     .setContentIntent(pi)
