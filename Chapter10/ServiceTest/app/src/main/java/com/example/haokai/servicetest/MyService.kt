5521531808d164fe600c521d2615668047180a0c
Hao Kai <haokai@njxds.com>
2020-07-17 16:54:08 +0800

    page 408

---
+++ Chapter10/ServiceTest/app/src/main/java/com/example/haokai/servicetest/MyService.kt
@@ -0,0 +1,42 @@
+package com.example.haokai.servicetest
+
+import android.app.Service
+import android.content.Intent
+import android.os.Binder
+import android.os.IBinder
+import android.util.Log
+
+class MyService : Service() {
+
+    private val mBinder = DownloadBinder()
+
+    class DownloadBinder : Binder() {
+        fun startDownload() {
+            Log.d("MyService", "startDownload executed")
+        }
+
+        fun getProcess(): Int {
+            Log.d("MyService", "getProgress executed")
+            return 0
+        }
+    }
+
+    override fun onBind(intent: Intent): IBinder {
+        return mBinder
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.d("MyService", "onCreate executed")
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        return super.onStartCommand(intent, flags, startId)
+        Log.d("MyService", "onStartCommand executed")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("MyService", "onDestroy executed")
+    }
+}
