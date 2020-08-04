7211064471cb4bd925bec67d5b661570574c7579
Kai <haokai.union@outlook.com>
2020-08-04 21:57:12 +0800

    page 538

---
+++ Chapter13/JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.jetpacktest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProviders
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        plusOneBtn.setOnClickListener {
+            viewModel.counter++
+            refreshCounter()
+        }
+        refreshCounter()
+    }
+
+    private fun refreshCounter() {
+        infoText.text = viewModel.counter.toString()
+    }
+
+
+}
