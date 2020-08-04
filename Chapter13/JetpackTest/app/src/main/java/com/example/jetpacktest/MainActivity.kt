5655ef72e53e32fc91f6721d623509efa0932006
Kai <haokai.union@outlook.com>
2020-08-04 22:23:58 +0800

    page 541

--- Chapter13/JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
+++ Chapter13/JetpackTest/app/src/main/java/com/example/jetpacktest/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.jetpacktest
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.edit
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProviders
 import kotlinx.android.synthetic.main.activity_main.*
@@ -9,21 +12,35 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
     lateinit var viewModel: MainViewModel
+    lateinit var sp: SharedPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        sp = getPreferences(Context.MODE_PRIVATE)
+        val countReserved = sp.getInt("count_reserved", 0)
+        viewModel = ViewModelProviders.of(this, MainViewModelFactory(countReserved))
+            .get(MainViewModel::class.java)
         plusOneBtn.setOnClickListener {
             viewModel.counter++
+            refreshCounter()
+        }
+        clearBtn.setOnClickListener{
+            viewModel.counter = 0
             refreshCounter()
         }
         refreshCounter()
     }
 
+    override fun onPause() {
+        super.onPause()
+        sp.edit { 
+            putInt("count_reserved", viewModel.counter)
+        }
+    }
+
     private fun refreshCounter() {
         infoText.text = viewModel.counter.toString()
     }
-
 
 }
