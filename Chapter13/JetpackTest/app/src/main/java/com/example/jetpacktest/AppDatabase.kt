3be8f6aaafb38ed3e90ceab5801de2bf4d935030
Hao Kai <haokai@njxds.com>
2020-08-07 16:04:52 +0800

    page 564

--- Chapter13/JetpackTest/app/src/main/java/com/example/jetpacktest/AppDatabase.kt
+++ Chapter13/JetpackTest/app/src/main/java/com/example/jetpacktest/AppDatabase.kt
@@ -4,13 +4,25 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(version = 1, entities = [User::class])
+@Database(version = 2, entities = [User::class, Book::class])
 abstract class AppDatabase : RoomDatabase(){
 
     abstract fun userDao(): UserDao
 
+    abstract fun bookDao(): BookDao
+
     companion object {
+
+        val MIGRATION_1_2 = object  : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("create table Book (id integer primary" +
+                        "key autoincrement not null, name text not null," +
+                        "pages integer not null)")
+            }
+        }
 
         private var instance: AppDatabase? = null
 
@@ -20,7 +32,9 @@ abstract class AppDatabase : RoomDatabase(){
                 return it
             }
             return Room.databaseBuilder(context.applicationContext,
-                AppDatabase::class.java, "app_database").build().apply {
+                AppDatabase::class.java, "app_database")
+                .addMigrations(MIGRATION_1_2)
+                .build().apply {
                 instance = this
             }
         }
